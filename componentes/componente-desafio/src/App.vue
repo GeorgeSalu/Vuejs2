7276d2c68d2ab33362cbaabddcbd147c503e636b
George Salu <george.salu10@gmail.com>
2019-06-04 20:10:39 -0300

    organiazndo os componentes em pastas

--- componentes/componente-desafio/src/App.vue
+++ componentes/componente-desafio/src/App.vue
@@ -14,10 +14,10 @@
 </template>
 
 <script>
-import Footer from './components/template/Footer.vue'
-import Carousel from './components/widgets/Carousel.vue'
-import Toolbar from './components/template/Toolbar.vue'
-import Menu from './components/template/Menu.vue'
+import Footer from '../components/template/Footer.vue'
+import Carousel from '../components/widgets/Carousel.vue'
+import Toolbar from '../components/template/Toolbar.vue'
+import Menu from '../components/template/Menu.vue'
 
 export default {
 	components: {'app-footer': Footer, 'app-carousel': Carousel, 'app-toolbar': Toolbar, 'app-menu': Menu},
