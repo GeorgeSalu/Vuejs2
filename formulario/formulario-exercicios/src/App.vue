6dc0cde268c5a0434c1973c360cfeae3e2933918
George Salu <george.salu10@gmail.com>
2019-06-16 10:08:15 -0300

    manipulando combobox com <select> e <option>

--- formulario/formulario-exercicios/src/App.vue
+++ formulario/formulario-exercicios/src/App.vue
@@ -28,8 +28,12 @@
 					<span><input type="radio" value="outros" v-model="produto"> Outro</span>
 				</Rotulo>
 				<Rotulo nome="Prioridade">
-					<select name="" id="">
-						<option></option>
+					<select v-model="prioridade">
+						<option v-for="prioridade in prioridades" 
+								:key="prioridade.codigo" 
+								:value="prioridade.codigo"
+								:selected="prioridade.codigo === 3">
+							{{ prioridade.nome }}</option>
 					</select>
 				</Rotulo>
 				<Rotulo nome="Primeira Reclamação?">
@@ -63,7 +67,7 @@
 					<span>{{ produto }}</span>
 				</Rotulo>
 				<Rotulo nome="Prioridade">
-					<span>???</span>
+					<span>{{ prioridade }}</span>
 				</Rotulo>
 				<Rotulo nome="Primeira Reclamação?">
 					<span>???</span>
@@ -85,6 +89,12 @@ export default {
 			mensagem: '',
 			caracteristicas: [],
 			produto: 'web',
+			prioridade: 1,
+			prioridades: [
+				{ codigo: 1, nome: 'Baixa' },
+				{ codigo: 2, nome: 'Moderada' },
+				{ codigo: 3, nome: 'Alta' }
+			],
 			usuario: {
 				email: '',
 				senha: '',
