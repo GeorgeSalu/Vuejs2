8925a03786f530f562dcd6415a58b4832b581e81
George Salu <george.salu10@gmail.com>
2019-06-16 10:25:10 -0300

    submentendo formulario

--- formulario/formulario-exercicios/src/App.vue
+++ formulario/formulario-exercicios/src/App.vue
@@ -2,7 +2,7 @@
 	<div id="app">
 		<h1>Registrar Reclamação</h1>
 		<div class="conteudo">
-			<form class="painel">
+			<form class="painel" v-if="!enviado">
 				<div class="cabecalho">Formulário</div>
 				<Rotulo nome="E-mail">
 					<input type="text" v-model.lazy.trim="usuario.email">
@@ -40,9 +40,9 @@
 					<Escolha v-model="escolha"/>
 				</Rotulo>
 				<hr>
-				<button>Enviar</button>
+				<button @click.prevent="enviar">Enviar</button>
 			</form>
-			<div class="painel">
+			<div class="painel" v-else>
 				<div class="cabecalho">Resultado</div>
 				<Rotulo nome="E-mail">
 					<span>{{ usuario.email }}</span>
@@ -84,6 +84,11 @@ import Escolha from './components/Escolha.vue'
 export default {
 	name: 'app',
 	components: { Rotulo, Escolha },
+	methods: {
+		enviar() {
+			this.enviado = true
+		}
+	},
 	data() {
 		return {
 			mensagem: '',
@@ -100,7 +105,8 @@ export default {
 				senha: '',
 				idade: 25
 			},
-			escolha: true
+			escolha: true,
+			enviado: false
 		}
 	},
 }
