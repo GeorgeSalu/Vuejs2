9ccf5ad8338dda55ebea12341b8f2a371a7f59f4
George Salu <george.salu10@gmail.com>
2019-06-28 22:00:14 -0300

    configurando de forma global o axios

--- http/http-exercicios/src/App.vue
+++ http/http-exercicios/src/App.vue
@@ -6,7 +6,12 @@
 
 <script>
 export default {
-
+	/*created() {
+		this.$http.post('usuarios.json', {
+			nome: 'maria',
+			email: 'maria_maria@gmail.com'
+		}).then(res => console.log(res))
+	}*/
 }
 </script>
 
