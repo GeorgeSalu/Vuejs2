5a266ca527cf6abdd28ad6b1a9bb6e9f914a50c1
George Salu <george.salu10@gmail.com>
2019-06-29 15:36:13 -0300

    implementando crud

--- http/http-exercicios/src/App.vue
+++ http/http-exercicios/src/App.vue
@@ -25,6 +25,11 @@
 				<strong>Nome : </strong> {{ usuario.nome }} <br>
 				<strong>E-mail : </strong> {{ usuario.email }} <br>
 				<strong>ID : </strong>{{id}}
+
+				<b-button variant="warning" size="lg"
+					@click="carregar(id)">Carregar</b-button>
+				<b-button variant="danger" size="lg" class="ml-2"
+					@click="excluir(id)">Excluir</b-button>
 			</b-list-group-item>
 		</b-list-group>
 	</div>
@@ -38,6 +43,7 @@ export default {
 	data() {
 		return {
 			usuarios: [],
+			id: null,
 			usuario: {
 				nome: '',
 				email: ''
@@ -45,12 +51,23 @@ export default {
 		}
 	},
 	methods: {
+		limpar() {
+			this.usuario.nome = ''
+			this.usuario.email = ''
+			this.id = null
+		},
+		carregar(id) {
+			this.id = id
+			this.usuario = { ...this.usuarios[id]}
+		},
+		excluir(id) {
+			this.$http.delete(`/usuarios/${id}.json`).then(() => this.limpar())
+		},
 		salvar() {
-			this.$http.post('usuarios.json', this.usuario)
-						.then(resp => {
-							this.usuario.nome = ''
-							this.usuario.email = ''
-						})
+			const metodo = this.id ? 'patch' : 'post'
+			const finalUrl = this.id ? `/${this.id}.json` : '.json'
+			this.$http[metodo](`/usuarios${finalUrl}`, this.usuario)
+				.then(_ => this.limpar())
 		},
 		obterUsuarios() {
 			this.$http.get('usuarios.json')
