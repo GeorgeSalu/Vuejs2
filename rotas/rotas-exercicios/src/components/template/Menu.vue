e21643178febf8ca2e99c6a73f0aaf8bc3636520
George Salu <george.salu10@gmail.com>
2019-06-30 18:05:47 -0300

    configurando rotas filhas (rotas aninhadas)

--- rotas/rotas-exercicios/src/components/template/Menu.vue
+++ rotas/rotas-exercicios/src/components/template/Menu.vue
@@ -5,8 +5,6 @@
                 <a>Inicio</a></router-link>
             <router-link to="/usuario/10" tag="li" active-class="active">
                 <a>Usuario</a></router-link>
-            <router-link to="/usuario/11" tag="li" active-class="active">
-                <a>Usuario</a></router-link>
         </ul>
     </nav>    
 </template>
---
+++ rotas/rotas-exercicios/src/components/usuario/UsuarioDetalhe.vue
@@ -0,0 +1,26 @@
+<template>
+    <div class="usuario-lista">
+        <h3>Usuario detalhe</h3>
+        <p><strong>Codigo:</strong>{{ id }}</p>
+    </div>
+</template>
+
+<script>
+export default {
+    props: ['id'],
+    /*data() {
+        return {
+            id: this.$route.params.id
+        }
+    },
+    watch: {
+        $route(to, from) {
+            this.id = to.params.id;
+        }
+    },*/
+}
+</script>
+
+<style>
+
+</style>
