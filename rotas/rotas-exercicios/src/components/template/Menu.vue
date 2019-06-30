6f6d61088a39bf349d9b35398270edf1e6e4d24c
George Salu <george.salu10@gmail.com>
2019-06-30 17:43:15 -0300

    lendo e usando paramentros de rotas

--- rotas/rotas-exercicios/src/components/template/Menu.vue
+++ rotas/rotas-exercicios/src/components/template/Menu.vue
@@ -5,6 +5,8 @@
                 <a>Inicio</a></router-link>
             <router-link to="/usuario/10" tag="li" active-class="active">
                 <a>Usuario</a></router-link>
+            <router-link to="/usuario/11" tag="li" active-class="active">
+                <a>Usuario</a></router-link>
         </ul>
     </nav>    
 </template>
