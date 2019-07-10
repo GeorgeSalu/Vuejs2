e2f8c524f29db6965dfd7da4d5cda8877d6bf189
George Salu <george.salu10@gmail.com>
2019-07-10 17:41:34 -0300

    configurando o vuex no projeto

--- projeto-03-acoes/stock-trader/src/App.vue
+++ projeto-03-acoes/stock-trader/src/App.vue
@@ -13,7 +13,11 @@
 import Header from './components/Header'
 
 export default {
-	components: { Header }
+	components: { Header },
+	created() {
+		this.$store.dispatch('initStocks')
+	}
+
 }
 </script>
 
