9c8a2cc5bd6aed9494089911aca462f199951008
George Salu <george.salu10@gmail.com>
2019-07-11 22:37:10 -0300

    exibindo saldo

--- projeto-03-acoes/stock-trader/src/components/Home.vue
+++ projeto-03-acoes/stock-trader/src/components/Home.vue
@@ -1,10 +1,28 @@
 <template>
-    <h1>Inicio</h1>
+    <div>
+        <h1 class="display-3 font-weight-light mb-4">Negocie e Consulte suas acoes</h1>
+        <v-sheet :elevation="6" class="pa-2 primary">
+            <v-icon class="white--text mr-3">Info</v-icon>
+            <span class="headline white--text font-weight-light">
+                Voce pode salvar e carregar os dados<span>
+        </v-sheet>
+        <v-sheet :elevation="6" class="pa-2 primary">
+            <v-icon class="white--text mr-3">Info</v-icon>
+            <span class="headline white--text font-weight-light">
+                Clique em 'finalizar Dia' para iniciar um novo dia<span>
+        </v-sheet>
+        <v-divider class="my-4"></v-divider>
+        <p class="display-1"><strong>Seu saldo:</strong>{{ funds }}</p>
+    </div>
 </template>
 
 <script>
 export default {
-    
+    computed: {
+        funds() {
+            return this.$store.getters.funds
+        }
+    }
 }
 </script>
 
