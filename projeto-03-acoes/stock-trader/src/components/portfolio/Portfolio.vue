9e341376e161b2bab298aebc4aaf513ff40d9fdc
George Salu <george.salu10@gmail.com>
2019-07-11 22:18:46 -0300

    conectando o portfolio ao vuex

--- projeto-03-acoes/stock-trader/src/components/portfolio/Portfolio.vue
+++ projeto-03-acoes/stock-trader/src/components/portfolio/Portfolio.vue
@@ -1,10 +1,20 @@
 <template>
-    <h1>Portfolio</h1>
+    <v-layout row wrap>
+        <Stock v-for="stock in stocks" :key="stock.id" :stock="stock" />
+    </v-layout>
 </template>
 
 <script>
+import Stock from './Stock'
+import { mapGetters } from 'vuex'
+
 export default {
-    
+    components: {Stock},
+    computed: {
+        ...mapGetters({
+            stocks: 'stockPortfolio'
+        })
+    }
 }
 </script>
 
