7baf9952c7fa616b9014598631ac59b423c0d7bb
George Salu <george.salu10@gmail.com>
2019-07-12 18:21:12 -0300

    incluindo css

--- projeto-04-calculadora/calculator/src/App.vue
+++ projeto-04-calculadora/calculator/src/App.vue
@@ -11,5 +11,22 @@ export default {
 </script>
 
 <style>
+    * {
+        font-family: 'Courier New', Courier, monospace
+    }
 
+    body {
+        margin: 0;
+    }
+
+    #app {
+        display: flex;
+        flex-direction: column;
+        height: 100vh;
+        justify-content: center;
+        align-items: center;
+        text-align: center;
+
+        color: #fff;
+    }
 </style>
