c4d911cea83bdd48b74b0c380ea5df2f59641c1b
George Salu <george.salu10@gmail.com>
2019-07-13 10:13:11 -0300

    inicio componente porta

---
+++ projeto-05-monty-hall/montyhall/src/components/Door.vue
@@ -0,0 +1,26 @@
+<template>
+
+</template>
+
+<script>
+import Gift from './Gift'
+
+export default {
+    name: 'Door',
+    components: { Gift },
+    props: {
+        number: {},
+        hasGift: {type: Boolean}
+    },
+    data: function() {
+        return {
+            open: false,
+            selected: false
+        }
+    }
+}
+</script>
+
+<style>
+
+</style>
