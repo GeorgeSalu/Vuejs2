98f8a76396889a92a815f6eeb5a00b756eb8df04
George Salu <george.salu10@gmail.com>
2019-07-13 10:24:16 -0300

    css porta

--- projeto-05-monty-hall/montyhall/src/components/Door.vue
+++ projeto-05-monty-hall/montyhall/src/components/Door.vue
@@ -1,5 +1,13 @@
 <template>
-
+    <div class="door-area">
+        <div class="door-frame">
+            <Gift v-if="open && hasGift"/>
+        </div>
+        <div class="door">
+            <div class="num">{{ number }}</div>
+            <div class="knob"></div>
+        </div>
+    </div>
 </template>
 
 <script>
@@ -22,5 +30,53 @@ export default {
 </script>
 
 <style>
+    :root {
+        --door-border: 5px solid brown;
+    }
 
+    .door-area {
+        position: relative;
+        width: 200px;
+        height: 310px;
+        border-bottom: 10px solid #AAA;
+        margin-bottom: 20px;
+        font-size: 3rem;
+        display: flex;
+        justify-content: center;
+    }
+
+    .door-frame {
+        position: absolute;
+        height: 300px;
+        width: 180px;
+
+        border-left: var(--door-border);
+        border-top: var(--door-border);
+        border-right: var(--door-border);
+
+        display: flex;
+        justify-content: center;
+        align-items: flex-end;
+    }
+
+    .door {
+        position: absolute;
+        top: 5px;
+        height: 295px;
+        width: 170px;
+        background-color: chocolate;
+
+        display: flex;
+        flex-direction: column;
+        padding: 20px;
+    }
+
+    .door .knob {
+        height: 20px;
+        width: 20px;
+        border-radius: 10px;
+        background-color: brown;
+        align-self: flex-start;
+        margin-top: 60px;
+    }
 </style>
