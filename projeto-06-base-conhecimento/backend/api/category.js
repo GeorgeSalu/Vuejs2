6bae4098130f0699448bcd4c9f9e591003ad7fd9
George Salu <george.salu10@gmail.com>
2019-07-17 17:26:01 -0300

    funcao para montar arvore

--- projeto-06-base-conhecimento/backend/api/category.js
+++ projeto-06-base-conhecimento/backend/api/category.js
@@ -95,5 +95,15 @@ module.exports = app => {
             .catch(err => res.status(500).send(err))
     }
 
+    const toTree = (categories, tree) => {
+        if(!tree) tree = categories.filter(c => !c.parentId)
+        tree = tree.map(parentNode => {
+            const isChild = node => node.parentId == parentNode.id
+            parentNode.children = toTree(categories, categories.filter(isChild))
+            return parentNode
+        })
+        return tree
+    }
+
     return { save, remove, get, getById }
 }
