21e71f68c754c52cfc1a840241429f4ee9ede27a
George Salu <george.salu10@gmail.com>
2019-07-19 10:34:41 -0300

    validando usuario administrador

--- projeto-06-base-conhecimento/backend/config/routes.js
+++ projeto-06-base-conhecimento/backend/config/routes.js
@@ -1,3 +1,5 @@
+const admin = require('./admin')
+
 module.exports = app => {
     app.post('/signup', app.api.user.save)
     app.post('/signin', app.api.auth.signin)
@@ -5,13 +7,13 @@ module.exports = app => {
 
     app.route('/users')
         .all(app.config.passport.authenticate())
-        .post(app.api.user.save)
-        .get(app.api.user.get)
+        .post(admin(app.api.user.save))
+        .get(admin(app.api.user.get))
 
     app.route('/users/:id')
         .all(app.config.passport.authenticate())
-        .put(app.api.user.save)
-        .get(app.api.user.getById)
+        .put(admin(app.api.user.save))
+        .get(admin(app.api.user.getById))
 
     app.route('/categories')
         .all(app.config.passport.authenticate())
@@ -31,13 +33,13 @@ module.exports = app => {
     app.route('/articles')
         .all(app.config.passport.authenticate())
         .get(app.api.article.get)
-        .post(app.api.article.save)
+        .post(admin(app.api.article.save))
 
     app.route('/articles/:id')
         .all(app.config.passport.authenticate())
         .get(app.api.article.getById)
-        .put(app.api.article.save)
-        .delete(app.api.article.remove)
+        .put(admin(app.api.article.save))
+        .delete(admin(app.api.article.remove))
 
     app.route('/categories/:id/articles')
         .all(app.config.passport.authenticate())
