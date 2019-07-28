93c25bc86099cbf7e30041d4780ffe197d3fead9
George Salu <george.salu10@gmail.com>
2019-07-28 20:26:18 -0300

    funcoes callback #03

--- bonus-javascript-essencial/callback2.js
+++ bonus-javascript-essencial/callback2.js
@@ -11,3 +11,11 @@ for(let i in notas) {
 console.log(notasBaixas)
 
 //com callback
+const notasBaixas2 = notas.filter(function(nota) {
+    return nota < 7
+})
+
+console.log(notasBaixas2)
+
+const notasBaixas3 = notas.filter(nota => nota < 7)
+console.log(notasBaixas3)
