a92ae572d51d9fe1e672971f5c2b7aecdbd84adf
George Salu <george.salu10@gmail.com>
2019-07-29 15:06:44 -0300

    esnext: revisao #01

---
+++ bonus-javascript-essencial/revisao1.js
@@ -0,0 +1,16 @@
+//let const
+{
+    var a = 1
+    let b = 3
+}
+
+console.log(a)
+//console.log(b) //erro
+
+//template string
+const produto = 'Ipad'
+console.log(`${produto} é caro`)
+
+//destructuring
+const [l, e, ...tras] = "Cod3r"
+console.log(l, e, tras)
