8de954f0859fcbb626bab8e7b36a61f08cdd6d28
George Salu <george.salu10@gmail.com>
2019-07-28 21:04:59 -0300

    desafio map

---
+++ bonus-javascript-essencial/map2.js
@@ -0,0 +1,6 @@
+const carrinho = [
+    '{ "nome": "borracha", "preco": 2.33 }',
+    '{ "nome": "caderno", "preco": 12.33 }',
+    '{ "nome": "kit de lapis", "preco": 4.33 }',
+    '{ "nome": "caneta", "preco": 22.33 }'
+]
