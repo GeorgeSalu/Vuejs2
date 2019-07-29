036beb3ef3deebe3dcf2925c6b4e28e5818e8a9c
George Salu <george.salu10@gmail.com>
2019-07-28 21:10:45 -0300

    desafio map

---
+++ bonus-javascript-essencial/map3.js
@@ -0,0 +1,18 @@
+Array.prototype.map2 = function(callback) {
+    
+}
+
+const carrinho = [
+    '{ "nome": "borracha", "preco": 2.33 }',
+    '{ "nome": "caderno", "preco": 12.33 }',
+    '{ "nome": "kit de lapis", "preco": 4.33 }',
+    '{ "nome": "caneta", "preco": 22.33 }'
+]
+
+//retornar um array apenas com os precos
+
+const paraObjeto = json => JSON.parse(json)
+const apenasPreco = produto => produto.preco
+
+const resultado = carrinho.map(paraObjeto).map(apenasPreco)
+console.log(resultado)
