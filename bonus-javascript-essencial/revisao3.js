4948b3146332d6a464be18ca87dce87cbf9b3375
George Salu <george.salu10@gmail.com>
2019-07-29 15:15:31 -0300

    esnext: revisao #03

---
+++ bonus-javascript-essencial/revisao3.js
@@ -0,0 +1,10 @@
+//es8: Object.values / Object.entries
+
+const obj = {a: 1, b: 2, c: 3}
+console.log(Object.values(obj))
+
+//melhoria notacao literal
+const nome = "carla"
+const pessoa = {
+    nome
+}
