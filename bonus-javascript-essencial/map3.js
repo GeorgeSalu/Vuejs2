3f3c8f707f42276578c9f10cac733dc02dc0a812
George Salu <george.salu10@gmail.com>
2019-07-28 21:13:04 -0300

    array: map #03

--- bonus-javascript-essencial/map3.js
+++ bonus-javascript-essencial/map3.js
@@ -1,5 +1,9 @@
 Array.prototype.map2 = function(callback) {
-    
+    const newArray = []
+    for(let i = 0;i < this.length; i++) {
+        newArray.push(callback(this[i], i, this))
+    }
+    return newArray
 }
 
 const carrinho = [
@@ -14,5 +18,5 @@ const carrinho = [
 const paraObjeto = json => JSON.parse(json)
 const apenasPreco = produto => produto.preco
 
-const resultado = carrinho.map(paraObjeto).map(apenasPreco)
+const resultado = carrinho.map2(paraObjeto).map2(apenasPreco)
 console.log(resultado)
