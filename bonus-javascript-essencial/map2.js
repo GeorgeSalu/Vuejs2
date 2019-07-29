d9b6ce19e8e620e7841dd71d1a8b7422c89db795
George Salu <george.salu10@gmail.com>
2019-07-28 21:08:30 -0300

    array: map #02

--- bonus-javascript-essencial/map2.js
+++ bonus-javascript-essencial/map2.js
@@ -4,3 +4,11 @@ const carrinho = [
     '{ "nome": "kit de lapis", "preco": 4.33 }',
     '{ "nome": "caneta", "preco": 22.33 }'
 ]
+
+//retornar um array apenas com os precos
+
+const paraObjeto = json => JSON.parse(json)
+const apenasPreco = produto => produto.preco
+
+const resultado = carrinho.map(paraObjeto).map(apenasPreco)
+console.log(resultado)
